059f1aad8a623402aeb12c253dbfdd0e88eceb7c
LordMidas <55047920+LordMidas@users.noreply.github.com>
2022-02-09 00:58:39 +0100

    Updated JS from Taro to stop squishing perks if Scrollable Perk Tree mod is detected

--- ui/mods/ptr_mod_screens.js
+++ ui/mods/ptr_mod_screens.js
@@ -187,16 +187,16 @@ CharacterScreenPerksModule.prototype.createPerkTreeDIV = function (_perkTree, _p
 {
     var self = this;
     scroll_createPerkTreeDIV.call(this, _perkTree, _parentDiv)
-    for (var row = 0; row < _perkTree.length; ++row)
-    {
-        if (_perkTree[row].length > 13)
+    if(this.mListScrollContainer == undefined){
+        for (var row = 0; row < _perkTree.length; ++row)
         {
-            var centerDiv = $('.center').eq( row )
-            centerDiv.find(".l-perk-container").css({ 'width': '4.0rem' });
-            centerDiv.css({ 'width': (4.0 * _perkTree[row].length) + 'rem' }); // css is dumb?
-            centerDiv.css({ 'left': ((660 - centerDiv.width()) / 2) + 'px' }); // css is dumb?
+            if (_perkTree[row].length > 13)
+            {
+                var centerDiv = $('.center').eq( row )
+                centerDiv.find(".l-perk-container").css({ 'width': '4.0rem' });
+                centerDiv.css({ 'width': (4.0 * _perkTree[row].length) + 'rem' }); // css is dumb?
+                centerDiv.css({ 'left': ((660 - centerDiv.width()) / 2) + 'px' }); // css is dumb?
+            }
         }
     }
-    
-
 };
